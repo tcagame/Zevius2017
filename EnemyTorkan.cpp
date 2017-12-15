98729bc25838a51d655a9826a2cb7e9729c36bf4
matuda <oreda>
2017-12-15 15:54:31 +0900

    matuda

--- EnemyTorkan.cpp
+++ EnemyTorkan.cpp
@@ -3,10 +3,20 @@
 #include "Drawer.h"
 #include "define.h"
 
+
 const Vector T_VEC( -3, 0 );
 const Vector REMOVE_POS( 300, 0 );
 const Vector REMOVE_VEC( 3, 3 );
-const double HARF_POS = 400;
+const double HALF_POS = 600;
+
+
+
+int Getrandom(int min, int max)
+{
+	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
+
+}
+
 
 EnemyTorkan::EnemyTorkan( const Vector& pos ) :
 Enemy( pos, 32, 500 ),
@@ -22,13 +32,17 @@ EnemyTorkan::~EnemyTorkan( ) {
 void EnemyTorkan::act( ) {
 	Vector pos = getPos( );
 	Vector vec = T_VEC;
+
+	
+
+	
 	if ( pos.x < REMOVE_POS.x && _remove == false ) {
 		_remove = true;
 	}
 	if ( _remove == true ) {
 		vec = REMOVE_VEC;
 	}
-	if ( pos.y > HARF_POS ) {
+	if ( pos.y < HALF_POS ) {
 		vec.y *= -1;
 	}
 
