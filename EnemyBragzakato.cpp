04764d6ee9fa2fb17ba5dc24ca1893a5c5fa31bb
yamakage <a@a.jp>
2017-12-15 14:11:30 +0900

    画鋲した。やったぜ。

--- EnemyBragzakato.cpp
+++ EnemyBragzakato.cpp
@@ -1,21 +1,32 @@
 #include "EnemyBragzakato.h"
+#include "Image.h"
+#include "Drawer.h"
+#include "define.h"
 
+const Vector ATTACK_POS( 50, 0 );
+const Vector T_VEC( -3, 0 );
 
-
-EnemyBragzakato::EnemyBragzakato( ) :
-Enemy( getPos( ), 32 , 30 ) {
-
+EnemyBragzakato::EnemyBragzakato( const Vector& pos ) :
+Enemy( pos, 32, 500 ) {
+	DrawerPtr drawer = Drawer::getTask( );
+	_image = drawer->createImage( "enemy/enemy.png" );
 }
 
-
+   
 EnemyBragzakato::~EnemyBragzakato( ) {
-
 }
 
 void EnemyBragzakato::act( ) {
+	Vector vec;
+	vec += T_VEC;
+	setVec( T_VEC );
 
+	draw( );
 }
 
 void EnemyBragzakato::draw( ) {
-
+	Vector pos = getPos( );
+	_image->setRect( NORMAL_GRAPH_SIZE * 1, 0, NORMAL_GRAPH_SIZE, NORMAL_GRAPH_SIZE );
+	_image->setPos( pos.x, pos.y );
+	_image->draw( );
 }
--- EnemyBragzakato.h
+++ EnemyBragzakato.h
@@ -1,11 +1,18 @@
 #pragma once
 #include "Enemy.h"
+#include "smart_ptr.h"
+
+PTR( Image );
+
 class EnemyBragzakato : public Enemy {
 public:
-	EnemyBragzakato( );
+	EnemyBragzakato( const Vector& pos );
 	virtual ~EnemyBragzakato( );
 private:
 	void act( );
 	void draw( );
+private:
+	ImagePtr _image;
+	Vector _pos;
 };
 
