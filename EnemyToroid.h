8e80e0b15549d2a41eee16f932c064baebf123e0
ishikawa <lolololololololololol>
2018-01-26 10:46:34 +0900

    とーろいど

--- EnemyToroid.h
+++ EnemyToroid.h
@@ -1,11 +1,18 @@
 #pragma once
 #include "Enemy.h"
+#include "smart_ptr.h"
+
+PTR( Image );
+
 class EnemyToroid : public Enemy {
 public:
-	EnemyToroid( );
+	EnemyToroid( const Vector& pos, ImagePtr small_2 );
 	virtual ~EnemyToroid( );
 private:
 	void act( );
-	void draw( );
+	void draw( int camera_x ) const;
+private:
+	ImagePtr _image;
+	int _count;
 };
 
